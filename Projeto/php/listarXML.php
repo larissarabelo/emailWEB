85895c3719fca19cdaedfab46ca9e9d68d008bbe
JulianoOB <55262443+JulianoOB@users.noreply.github.com>
2019-11-16 14:26:26 -0300

    Cadastro erro XML
    Nao retornava o email e a senha, mudei para retornar true, pelo php.

--- Projeto/php/listarXML.php
+++ Projeto/php/listarXML.php
@@ -1,12 +1,18 @@
 <?php
-    
-    $xml_string = file_get_contents("../xml/dados.xml");
+    if($_POST){
+        extract($_POST);
+            
+        $xml_string = file_get_contents("../xml/dados.xml");
+        $xml_objeto = simplexml_load_string($xml_string);
 
-    $xml_objeto = simplexml_load_string($xml_string);
-
-    $retorno["email"] = trim($xml_objeto->email);
-    $retorno["senha"] = trim($xml_objeto->senha);
-
-    echo json_encode($retorno);
-
+        if($xml_objeto->email == $email && $xml_objeto->senha == $senha){
+            echo json_encode(true))
+        }
+        else{
+            echo json_encode(false);
+        }
+    }
+    else{
+        echo json_encode(false);
+    }
 ?>
